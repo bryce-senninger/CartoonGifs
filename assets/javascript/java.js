80cc9b98c0d4eb15dc8436c125630b2090a241bf
Bryce <bryce.senninger@gmail.com>
2019-06-21 16:02:48 -0600

    updates to pause functionality, still in progress

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -11,7 +11,6 @@ function displayCartoon() {
         })
             .then(function (response) {
                 var results = response.data;
-                console.log(response);
                 $("#gifs-go-here").empty();
                 for (var i = 0; i < results.length; i++) {
                     var gifDiv = $("<div>");
@@ -19,13 +18,16 @@ function displayCartoon() {
                     var p = $("<p>").text("Rating: " + rating);
                     var personImage = $("<img>");
                     personImage.attr("src", results[i].images.fixed_height.url);
+                    personImage.addClass("gif");
                     gifDiv.prepend(p);
                     gifDiv.prepend(personImage);
                     $("#gifs-go-here").prepend(gifDiv);
-                    console.log(response.data)
+                    setDataState();
                 }
             });
 }
+
+
 
 
 
@@ -52,3 +54,29 @@ $(document).on("click", ".character-btn", displayCartoon);
 
 
 renderButtons();
+
+
+
+
+//==================================================================//
+//Stuff below here related to pause functionality still needs work- setting a data state for all loaded gifs 
+//as they appear by calling the below global function, started toggle pause function
+//based around state attribute recognition and "if" statement that will switch the source for the image and the
+//"state" attribute. Need to figure out how to reference the image url location without using the for loop variable...
+
+function setDataState() {
+    let cartoonImage = $(".gif");
+    cartoonImage.attr("state", "animate");
+}
+
+// function togglePause() {
+//     if ($(this).attr("state" === "animate")) {
+//         $(this).attr("src", results[i].images.fixed_height_still.url);
+//         $(this).attr("state", "still");
+//     } else {
+//         $(this).attr("src", results[i].images.fixed_height.url);
+//         $(this).attr("state", "animate");
+//     }
+// }
+
+// $(".gif").on("click", togglePause());
